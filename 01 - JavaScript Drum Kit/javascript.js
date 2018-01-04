9d01a7a3a2c80624c03cafa9664530ec91722961
Manuel Torres <manueljosetorres02@gmail.com>
2018-01-03 23:46:50 -0600

    work through tutorial to  7:38
    Work through the tutorial to 7:38.
    
    This includes adding the window event.
    adding the const variable - and the if the variable is not working/returns null to stop the function form working until a new keydown event takes place.
    
    Audio.play will play the sound on the keydown event

---
+++ 01 - JavaScript Drum Kit/javascript.js
@@ -0,0 +1,5 @@
+// need to do an onload for a separate javascript file to work 
+//Listen for keydown event - when the event happens - run the function
+window.addEventListener('keydown', function(element) {
+console.log(element);
+})
